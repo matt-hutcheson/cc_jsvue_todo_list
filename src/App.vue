a367fad44593492a95af1e607fa157f47111ff39
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-10-19 16:39:49 +0100

    initial commit. MVP + radiobuttons working

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>ToDo's</h1>
+    <form v-on:submit.prevent="newToDo">
+      <input type="text" id="new-item" required v-model="newItem">
+      <label for="high">High</label>
+      <input type="radio" value="high" name="importance" id="high" v-model="radioButtonValue">
+      <label for="low">Low</label>
+      <input type="radio" value="low" name="importance" id="low" v-model="radioButtonValue" checked>
+      <input type="submit" value="Save Item">
+    </form>
+    <ul >
+      <li v-for="(item, index) in items" v-bind:key="index">
+        {{item.name}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      items: [
+        {name: "Buy shopping", important: false},
+        {name: "Clean bathroom", important: true},
+        {name: "Car's MOT", important: false}
+        ],
+      newItem: "",
+      radioButtonValue: "low"
+    }
+  },
+  methods: {
+    newToDo: function () {
+      this.items.push(
+        {name: this.newItem, important: (this.radioButtonValue === 'high' ? true: false)}
+      );
+      this.newItem = "";
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
