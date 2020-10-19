bc594452502a5dc936f37b8a53478c476cde1d6a
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-10-19 19:15:30 +0100

    added css and high priority visuals

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,13 @@
       <input type="submit" value="Save Item">
     </form>
     <ul >
-      <li v-for="(item, index) in items" v-bind:key="index">
-        {{item.name}}
+      <li v-for="(item, index) in items" v-bind:key="index" v-bind:class="item.important ? 'priority': 'not-priority'">
+        <span>
+          {{item.name}}
+        </span>
+        <span v-if="item.important">
+          High Priority!
+        </span>
       </li>
     </ul>
   </div>
@@ -42,5 +47,47 @@ export default {
 </script>
 
 <style>
+#app {
+  width: 60%;
+  margin: 0 auto;
+  font-family: 'Lato', sans-serif;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+
+li {
+  margin: 20px 0;
+  padding: 10px;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+}
+
+li.not-priority {
+  border: 2px solid #1a681e;
+  color: #1a681e;
+}
+
+li.priority {
+  border: 2px solid #f2360c;
+}
+
+input[type="text"] {
+  padding: 10px;
+  width: 50%;
+  margin:5px;
+}
+
+input[type="submit"]{
+  padding: 10px;
+  background: #000;
+  color: #fff;
+  cursor: pointer;
+  border: 1px solid #000;
+}
 
 </style>
